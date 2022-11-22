bc09b3f968adfc32f1df8a1b9985a93378fc1d7f
bigmb <malav.b93@gmail.com>
2022-11-22 03:13:32 +0000

    adding files

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import setup,find_packages,find_namespace_packages
+from mb_utils.src.version import version
+
+setup(
+    name="mb_pandas",
+    version=version,
+    description="Basic Pandas functions package",
+    author=["Malav Bateriwala"],
+    packages=find_namespace_packages(include=["mb_pandas.*"]),
+    #packages=find_packages(),
+    scripts=[],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "colorama",],
+    python_requires='>=3.8',)
