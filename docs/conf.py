c71920c8a95c67c871774ccfd512044d2e9eefdf
bigmb <malav.b93@gmail.com>
2022-11-28 05:38:20 +0000

    rmd autoapi

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,7 @@ release = version
 # -- General configuration ---------------------------------------------------
 
 import sphinx_rtd_theme
+import autoapi
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
