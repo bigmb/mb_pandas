709c9999b8478248b8eb7e2d6c2734907a30d693
bigmb <malav.b93@gmail.com>
2022-11-28 05:39:25 +0000

    stupid pytohn

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,6 @@ release = version
 # -- General configuration ---------------------------------------------------
 
 import sphinx_rtd_theme
-import autoapi
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
