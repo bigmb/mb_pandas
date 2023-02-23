3a11dbd59d466e471214cf77709897b2227435be
bigmb <malav.b93@gmail.com>
2023-02-23 21:06:42 +0000

    added async parquet support

--- mb_pandas/src/dfload.py
+++ mb_pandas/src/dfload.py
@@ -37,6 +37,29 @@ async def load_df_new(fp,show_progress=False):
     data1 = await read_txt(fp) 
     return process(fp , io.StringIO(data1)) 
 
+async def load_df_new_parquet(fp,show_progress=False): 
+    """
+    load pandas dataframe from csv file using asyncio
+    Input:
+        fp (str): path to csv file
+        show_progress (bool): show progress bar
+    Output:
+        df (pd.DataFrame): pandas dataframe
+    """
+    def process(fp,data1: io.StringIO):
+        if show_progress:
+            bar = tqdm(unit='row') 
+        dfs=[] 
+        for df in pd.read_parquet(data1,chunksize=1024): 
+            dfs.append(df) 
+            if show_progress:
+                bar.update(len(df))
+        df = pd.concat(dfs,sort=False) 
+        if show_progress:
+            bar.close()
+        return df 
+    data1 = await read_txt(fp) 
+    return process(fp , io.StringIO(data1)) 
 
 def load_any_df(file_path,show_progress=True,logger = None):
     """
@@ -48,7 +71,10 @@ def load_any_df(file_path,show_progress=True,logger = None):
         df (pd.DataFrame): pandas dataframe
     """
     try:
-        df = asyncio.run(load_df_new(file_path,show_progress=show_progress))
+        if file_path.endswith('.csv'):
+            df = asyncio.run(load_df_new(file_path,show_progress=show_progress))
+        elif file_path.endswith('.parquet'):
+            df = asyncio.run(load_df_new_parquet(file_path,show_progress=show_progress))
         if logger:
             logger.info("Loaded dataframe from {} using asyncio".format(file_path))
     except:
