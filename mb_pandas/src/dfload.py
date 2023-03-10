8e64604f6eef1634f781499e54dcca00da279679
bigmb <malav.b93@gmail.com>
2023-03-10 04:18:52 +0000

    big fix

--- mb_pandas/src/dfload.py
+++ mb_pandas/src/dfload.py
@@ -78,14 +78,8 @@ def load_any_df(file_path,show_progress=True,max_rows=None, logger = None):
         if logger:
             logger.info("Loaded dataframe from {} using asyncio".format(file_path))
     except:
-        if file_path.endswith('.csv'):
-            df = pd.read_csv(file_path, index_col=0)
-        elif file_path.endswith('.parquet'):
-            df = pd.read_parquet(file_path)
-        else:
-            raise ValueError("File type not supported")
-        if logger:
-            logger.info("Loaded dataframe from {} using pandas".format(file_path))
+        raise ValueError("File type not supported")
+    
     #df = df.reset_index(drop=True)
     df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
     return df
