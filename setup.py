e7898b334e6262ff72d5d2074e6b2543dfa73637
Malav Bateriwala <malav.bateriwala@winnowsolutions.com>
2023-06-06 13:38:33 +0100

    Bug fix commit

--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@
 
 from setuptools import setup,find_packages,find_namespace_packages
 from mb_pandas.src.version import version
+import sys
+
+py_version = sys.version
+print(py_version)
+if py_version[:4] == '3.10':
+    py_requires = '>=3.10'
+else:
+    py_requires = '>=3.8'
+print(py_requires)
 
 setup(
     name="mb_pandas",
@@ -16,4 +25,4 @@ setup(
         "numpy",
         "pandas",
         "colorama",],
-    python_requires='>=3.8',)
+    python_requires=py_requires,)
