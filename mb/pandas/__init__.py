9b7609d7ee5ff3c08ef9ebb25f5feed33c81f077
bigmb <b.malav93@gmail.com>
2026-02-17 12:21:21 +0000

    updated version.py to 2.0.10

---
+++ mb/pandas/__init__.py
@@ -0,0 +1,8 @@
+from pandas import *
+
+# Custom mb.pandas utilities
+from mb.pandas.dfload import load_any_df
+from mb.pandas.convert_data import *
+from mb.pandas.profiler import *
+from mb.pandas.transform import *
+from mb.pandas.aio import *
